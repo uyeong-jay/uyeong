5b3298fbe1fe3b4a6ad5e3c5ad9e00fc35e3be16
william-jacob <wjacob2103@gmail.com>
2022-01-21 10:15:27 +0900

    feat: set jest

---
+++ front/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '@src/styles/globals.css';
+
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  console.log('a');
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
