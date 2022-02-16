725b631b8cb1571e2386663a10da16917cf532a9
william-jacob <wjacob2103@gmail.com>
2022-02-16 10:40:23 +0900

    feat: added code and blockquote

--- front/src/components/UI/organisms/MarkdownViewer/components/index.ts
+++ front/src/components/UI/organisms/MarkdownViewer/components/index.ts
@@ -1,8 +1,10 @@
-import CodeBlock from './CodeBlock';
 import { Components } from 'react-markdown/lib/ast-to-react';
+import code from './code';
+import blockquote from './blockquote';
 
 const components: Components = {
-  code: CodeBlock,
+  code,
+  blockquote,
 };
 
 export default components;
