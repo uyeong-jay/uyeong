02606ce5004b9435ff8b97be5ac8414e5bb081ac
william-jacob <wjacob2103@gmail.com>
2022-02-16 10:37:25 +0900

    feat: added blockquote component

---
+++ front/src/components/UI/organisms/MarkdownViewer/components/blockquote.tsx
@@ -0,0 +1,18 @@
+import styled from '@_settings/styled';
+
+interface BlockQuoteProps {
+  children: React.ReactNode;
+}
+
+const Blockquote = styled.blockquote`
+  color: #666;
+  margin: 0;
+  padding-left: 3em;
+  border-left: 0.5em #6ff9ff solid;
+`;
+
+const blockquote = ({ children }: BlockQuoteProps) => {
+  return <Blockquote>{children}</Blockquote>;
+};
+
+export default blockquote;
