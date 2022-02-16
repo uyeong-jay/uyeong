55e24d02a69fea65cd9ca3ce2558af0c0458822c
william-jacob <wjacob2103@gmail.com>
2022-02-16 10:45:41 +0900

    style: put the types on top

--- front/src/components/UI/organisms/MarkdownViewer/MarkdownViewer.tsx
+++ front/src/components/UI/organisms/MarkdownViewer/MarkdownViewer.tsx
@@ -2,13 +2,13 @@ import styled from '@_settings/styled';
 import ReactMarkdown from 'react-markdown';
 import components from './components';
 
-const MarkdownWrapper = styled.div`
-  font-size: 14px;
-`;
-
 interface Props {
   content: string;
 }
+
+const MarkdownWrapper = styled.div`
+  font-size: 14px;
+`;
 
 const MarkdownViewer = ({ content }: Props) => {
   return (
