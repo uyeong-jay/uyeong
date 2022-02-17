f083751bbc763d7f7797d9844de95a91048078d2
william-jacob <wjacob2103@gmail.com>
2022-02-17 15:46:56 +0900

    feat: added paragraph

--- front/src/components/UI/organisms/MarkdownViewer/components/index.ts
+++ front/src/components/UI/organisms/MarkdownViewer/components/index.ts
@@ -1,10 +1,12 @@
 import { Components } from 'react-markdown/lib/ast-to-react';
 import code from './code';
 import blockquote from './blockquote';
+import paragraph from './paragraph';
 
 const components: Components = {
   code,
   blockquote,
+  p: paragraph,
 };
 
 export default components;
