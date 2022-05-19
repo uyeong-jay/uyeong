25df056d713cb6c658a4cccf41b8587fe23afb58
william-jacob <wjacob2103@gmail.com>
2022-05-19 16:57:27 +0900

    chore: delete husky

---
+++ server/lint-staged.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // 타입스크립트 파일만 타입체크 해주기
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // 타입스크립트와 자바스크립트 파일만 린트, 포맷 해주기(배열: 순서대로 실행(린트 후 포맷 됨))
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // 마크다운과 JSON 파일만 포맷 해주기
+  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
+};
