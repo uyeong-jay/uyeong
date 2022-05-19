2c24fa15b9d31c28f7bb1106545f2dc77659e9fa
william-jacob <wjacob2103@gmail.com>
2022-05-19 16:17:57 +0900

    feat: added theme

---
+++ client/src/components/UI/_settings/theme/index.ts
@@ -0,0 +1,28 @@
+import '@emotion/react';
+
+const breakpoint = {
+  FHDPC: '1400px',
+  HDPC: '1200px',
+  PC: '992px',
+  TABLET: '768px',
+  MOBILE: '576px',
+};
+
+export const lightTheme = {
+  BACKGROUND_COLOR: '#ffffff',
+  BP: breakpoint,
+};
+
+export const darkTheme = {
+  BACKGROUND_COLOR: '#0D1117',
+  COLOR: 'white',
+  BP: breakpoint,
+};
+
+declare module '@emotion/react' {
+  export interface Theme {
+    BACKGROUND_COLOR: string;
+    COLOR: string;
+    BP: typeof breakpoint;
+  }
+}
