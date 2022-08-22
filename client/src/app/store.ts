dc6bd9e65e713cadad0182485708371db4cc81fb
william-jacob <wjacob2103@gmail.com>
2022-08-22 16:17:26 +0900

    chore: initial setting redux toolkit

---
+++ client/src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'; //리듀서들 + 미들웨어
+import reduxLogger from 'redux-logger'; //미들웨어
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(reduxLogger),
+});
+
+//+ Add types
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,15 +3,20 @@ import AppLayout from '@templates/AppLayout';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 
+import { Provider } from 'react-redux';
+import store from '@app/store';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* fontawsome icon  */}
       <Script src={`${process.env.FONTAWESOME_KIT}`} crossOrigin="anonymous"></Script>
       <GlobalStyle />
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
+      <Provider store={store}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Provider>
     </>
   );
 }
