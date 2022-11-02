7da53ef7a52b056e6d0221d89ccaebc4d7928d37
william-jacob <wjacob2103@gmail.com>
2022-11-02 16:22:59 +0900

    feat: added uploading image feature

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,9 @@ module.exports = {
   env: {
     BASE_URL: prod ? '' : process.env.BASE_URL,
     FONTAWESOME_KIT: process.env.FONTAWESOME_KIT,
+    CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API: process.env.CLOUDINARY_API,
   },
 
   images: {
