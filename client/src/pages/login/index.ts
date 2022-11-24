2188b6062113552460bc33ea507bb2e4458039c8
william-jacob <wjacob2103@gmail.com>
2022-11-24 17:20:25 +0900

    refactor: refactored login page

--- client/src/pages/login/index.ts
+++ client/src/pages/login/index.ts
@@ -3,7 +3,7 @@ import wrapper from '@app/store';
 import axios from 'axios';
 import { getRunningOperationPromises, getUserData } from '@app/services/userApi';
 
-export { default } from '@pages/Login';
+export { default } from '@pages/Login/LoginPage';
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
   const cookie = context.req ? context.req.headers.cookie : '';
