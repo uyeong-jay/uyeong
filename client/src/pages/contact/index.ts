dbb098b994f647ed01e588450bbb22a85b1e9fb6
william-jacob <wjacob2103@gmail.com>
2022-11-24 17:23:54 +0900

    refactor: refactored contact page

--- client/src/pages/contact/index.ts
+++ client/src/pages/contact/index.ts
@@ -3,7 +3,7 @@ import wrapper from '@app/store';
 import axios from 'axios';
 import { getRunningOperationPromises, getUserData } from '@app/services/userApi';
 
-export { default } from '@pages/Contact';
+export { default } from '@pages/Contact/ContactPage';
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
   const cookie = context.req ? context.req.headers.cookie : '';
