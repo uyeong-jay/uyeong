9a520a76067597ae33664d10897f626b0496f214
william-jacob <wjacob2103@gmail.com>
2022-12-24 21:35:24 +0900

    feat: added initial valid blog utill

---
+++ client/src/utils/valid/validBlog.ts
@@ -0,0 +1,25 @@
+interface blogPostInfoProps {
+  user: string;
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnail: string;
+  description: string;
+  category: string;
+  createdAt: string;
+}
+
+const validBlog = (blogPostInfo: blogPostInfoProps) => {
+  const { title, content } = blogPostInfo;
+
+  //title 에러
+  if (!title) return 'Please add title.';
+  else if (title.length > 2) return 'Title must be 50 characters or less.';
+
+  //content 에러
+  if (!content) return 'Please add content.';
+  else if (content.length > 4000) return 'Content must be 50 characters or less.';
+  return '';
+};
+
+export default validBlog;
