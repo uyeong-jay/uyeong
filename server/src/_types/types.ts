ea4a2f2cb2d32cc5c6b0b0550cb4d6cdcb98166b
william-jacob <wjacob2103@gmail.com>
2023-01-04 09:45:36 +0900

    refactor: refactor server type & apply new prettier

--- server/src/_types/types.ts
+++ server/src/_types/types.ts
@@ -1,39 +1,39 @@
 import { Request } from "express";
-import mongoose from "mongoose";
+import { Types } from "mongoose";
 
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const ObjectId = Types.ObjectId;
 
 export interface IUser {
-	_id: typeof ObjectId;
-	nickname: string;
-	email: string;
-	password: string;
-	cf_password: string;
-	avatar?: string;
-	role: string;
-	type: string;
-	_doc: object; 
+  _id: typeof ObjectId;
+  nickname: string;
+  email: string;
+  password: string;
+  cf_password: string;
+  avatar?: string;
+  role: string;
+  type: string;
+  _doc: object;
 }
 
 export interface IReqAuth extends Request {
-	user?: IUser;
+  user?: IUser;
 }
 
 //token 디코드시 얻는 데이터
 export interface IDecodedToken {
-	id?: string;
-	iat: number;
-	exp: number;
+  id?: string;
+  iat: number;
+  exp: number;
 }
 
-export interface IBlog {
-	_id: typeof ObjectId;
-	user: string;
-	title: string;
-	tags: [String];
-	content: string;
-	description: string;
-	thumnail: string;
-	category: string;
-	_doc: object;
+export interface IPost {
+  _id: typeof ObjectId;
+  user: string;
+  title: string;
+  tags: [String];
+  content: string;
+  description: string;
+  thumnail: string;
+  category: string;
+  _doc: object;
 }
