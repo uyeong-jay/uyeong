458d1379c81c1b7e7242f3e96abff8b853ebb6c1
william-jacob <wjacob2103@gmail.com>
2023-01-20 18:24:58 +0900

    feat: added nextjs dynamic for lazy load with type

--- client/src/components/UI/organisms/MarkdownViewer/MarkdownViewer.tsx
+++ client/src/components/UI/organisms/MarkdownViewer/MarkdownViewer.tsx
@@ -1,9 +1,9 @@
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import components from './components';
-import { lazy, Suspense } from 'react';
 import Loader from '@modals/Loader';
-const ReactMarkdown = lazy(() => import('react-markdown'));
+import dynamic from 'next/dynamic';
+const ReactMarkdown: any = dynamic(() => import('react-markdown') as any, { loading: () => <Loader />, ssr: false });
 
 interface Props {
   content: string;
@@ -11,11 +11,9 @@ interface Props {
 
 const MarkdownViewer = ({ content }: Props) => {
   return (
-    <Suspense fallback={<Loader />}>
-      <ReactMarkdown components={components} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
-        {content}
-      </ReactMarkdown>
-    </Suspense>
+    <ReactMarkdown components={components} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+      {content}
+    </ReactMarkdown>
   );
 };
 
