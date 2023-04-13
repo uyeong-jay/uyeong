e579bdb3a1badef4e606eac4e19e80acd4ac2b91
william-jacob <wjacob2103@gmail.com>
2023-04-13 18:31:37 +0900

    rename: edit file name

--- server/src/routes/blog/postRouter.ts
+++ server/src/routes/blog/postRouter.ts
@@ -3,14 +3,14 @@ import blogPostCtrl from "@controllers/blogCtrl/postCtrl";
 import auth from "@middleware/auth";
 
 const blogPostRouter = express.Router();
-const { getPosts, getPost, getPostsByCategory, getPostsBySeatch, createPost, updatePost, deletePost } = blogPostCtrl;
+const { getPosts, getPost, getPostsByCategory, getPostsBySearch, createPost, updatePost, deletePost } = blogPostCtrl;
 
 // get(read): 데이터 조회, no need data
 // post(create): 데이터 등록
 // put(update): 데이터 변경(모두)
 // patch(update): 데이터 변경(부분)
 // delete(delete): 데이터 제거
-blogPostRouter.get("/search", getPostsBySeatch);
+blogPostRouter.get("/search", getPostsBySearch);
 blogPostRouter.get("/blog", getPosts);
 blogPostRouter.get("/blog/:slug", getPost);
 blogPostRouter.get("/blog/category/:slug", getPostsByCategory);
