015d0e9b476256f2cb0aa9d4895359e7af65cad0
william-jacob <wjacob2103@gmail.com>
2023-12-20 02:31:56 +0900

    design: added initial post card ui

--- client/src/components/pages/Blog/BlogComponents/BlogPosts/BlogPosts.tsx
+++ client/src/components/pages/Blog/BlogComponents/BlogPosts/BlogPosts.tsx
@@ -1,33 +1,97 @@
 import { memo } from 'react';
-import { useAppSelector } from '@app/hooks';
 import { BlogPostRes } from '@app/services/blog/postApi';
 import BlogPostCard from '../BlogPostCard';
 import styled from '@_settings/styled';
+import { v4 as uuid } from 'uuid';
 
 interface Propss {
-  blogPostsData?: BlogPostRes;
   blogPostsBySearch?: BlogPostRes;
 }
 
-const DIV = {} as any;
+export const DIV = {} as any;
 
 DIV.BlogPostsWrapper = styled.div`
   // border: 1px solid black;
 `;
 
-const BlogPosts = ({ blogPostsData, blogPostsBySearch }: Propss) => {
-  const tagName = useAppSelector((state) => state.blog.tagName);
+DIV.NoPostsFound = styled.div`
+  // border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.FONT_C};
+  font-weight: bold;
+  font-size: 14px;
+  height: 200px;
+`;
 
+DIV.InitialPostsCard = styled.div`
+  border: 2px solid ${({ theme }) => theme.BD_C};
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  min-width: ${({ theme }) => theme.BP.MOBILE};
+  width: ${({ theme }) => theme.BP.MOBILE};
+  height: 500px;
+  margin-bottom: 130px;
+
+  @media screen and (min-width: calc(${({ theme }) => theme.BP.MOBILE} + 100px)) {
+    border: none;
+  }
+
+  @media screen and (min-width: calc(${({ theme }) => theme.BP.TABLET})) {
+    width: 450px;
+  }
+
+  @media screen and (min-width: calc(${({ theme }) => theme.BP.TABLET} + 300px)) {
+    border: 2px solid ${({ theme }) => theme.BD_C};
+    border-radius: 20px;
+    flex-direction: row;
+    width: 600px;
+    height: 300px;
+    min-height: 300px;
+  }
+  @media screen and (min-width: ${({ theme }) => theme.BP.PC}) {
+    width: 800px;
+    height: 400px;
+  }
+`;
+
+DIV.NoMorePosts = styled.div`
+  // border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.FONT_C};
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+const POSTCOUNT = 4;
+export const InitialPostsCardArr = Array.from({ length: POSTCOUNT }, (_v, index) => index);
+
+const BlogPosts = ({ blogPostsBySearch }: Propss) => {
   return (
     <DIV.BlogPostsWrapper>
-      <div>{!blogPostsData && <div>No Posts!</div>}</div>
-      {/* client-(검색된)포스트(o,x) or server-(일반)포스트 */}
       {blogPostsBySearch?.posts ? (
-        blogPostsBySearch?.posts?.map((post) => <BlogPostCard key={post._id} post={post} />)
-      ) : blogPostsBySearch?.msg === 'No blogs' ? (
-        <div>검색된 블로그가 없습니다.</div>
+        blogPostsBySearch?.posts?.map((post) =>
+          post ? <BlogPostCard key={post._id} post={post} /> : <div key={uuid()}></div>,
+        )
+      ) : blogPostsBySearch?.matchingPosts === false ? (
+        <DIV.NoPostsFound>- No posts found in the search -</DIV.NoPostsFound>
       ) : (
-        !tagName && blogPostsData?.posts?.map((post) => <BlogPostCard key={post._id} post={post} />)
+        //새로고침시 보이는 화면 (dynamic 적용 전)
+        <>
+          {InitialPostsCardArr.map((cardCountIndex) => (
+            <DIV.InitialPostsCard key={cardCountIndex}></DIV.InitialPostsCard>
+          ))}
+        </>
+      )}
+      {!blogPostsBySearch?.next_cursor && blogPostsBySearch?.matchingPosts && (
+        <DIV.NoMorePosts>- No more posts -</DIV.NoMorePosts>
       )}
     </DIV.BlogPostsWrapper>
   );
