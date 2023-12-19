3e639857fe5dd59ef9d18ac6159a7efb28f5e337
william-jacob <wjacob2103@gmail.com>
2023-12-20 02:35:08 +0900

    feat: added scroll event in intersect hook

--- client/src/hooks/useIntersect.ts
+++ client/src/hooks/useIntersect.ts
@@ -1,9 +1,11 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
 
 type IntersectHandler = (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void;
 
-export const useIntersect = (onIntersect: IntersectHandler, options?: IntersectionObserverInit) => {
+export const useIntersect = (onIntersect: IntersectHandler, options?: IntersectionObserverInit, elementId?: string) => {
   const ref = useRef<HTMLDivElement>(null);
+  const [targetRef, setTargetRef] = useState(ref.current);
+  const [isScrolled, setScrolled] = useState(false);
   const callback = useCallback(
     (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
       entries.forEach((entry) => {
@@ -14,11 +16,25 @@ export const useIntersect = (onIntersect: IntersectHandler, options?: Intersecti
   );
 
   useEffect(() => {
-    if (!ref.current) return;
+    if (!ref.current) {
+      //다른 페이지에서 새로고침 후 해당 페이지로 올때 실행됨
+      //next/dynamic 적용시 필요
+      const ElementById = document.getElementById(elementId || '') as SetStateAction<HTMLDivElement | null>;
+      const handleScroll = () => {
+        if (!isScrolled) {
+          setScrolled(true);
+          setTargetRef(ElementById);
+        }
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
     const observer = new IntersectionObserver(callback, options);
-    observer.observe(ref.current);
+    observer.observe(ref.current || targetRef);
     return () => observer.disconnect();
-  }, [ref, callback, options]);
+  }, [ref, callback, options, isScrolled, elementId, targetRef]);
 
   return ref;
 };
