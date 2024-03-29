6e423a9d0a42ec9e76cb965971f6ef71ca54c5b8
william-jacob <wjacob2103@gmail.com>
2024-03-29 14:24:20 +0900

    feat: limit the number of characters

--- server/src/controllers/blogCtrl/categoryCtrl/updateCategory.ts
+++ server/src/controllers/blogCtrl/categoryCtrl/updateCategory.ts
@@ -5,7 +5,7 @@ import Posts from "@models/blog/postModel";
 
 const updateCategory = async (req: IReqAuth, res: Response) => {
   try {
-    //user가 middleware auth를 잘통과 했는지 확인
+    //user가 middleware auth를 통과 했는지 확인
     if (!req.user) return res.status(400).json({ msg: "Invalid Authorization." });
 
     //admin인지 확인
@@ -14,12 +14,14 @@ const updateCategory = async (req: IReqAuth, res: Response) => {
     //client 데이터 가져오기
     const { name: oldName, currName } = req.body;
 
+    if (!currName.length) return res.status(400).json({ msg: "The category name must be more than 1 character." });
+
     if (currName.length > 20)
       return res.status(400).json({ msg: "The category name must be less than 20 characters." });
 
     //category 조회(중복 조회)
-    const category = await Categories.findOne({ name: currName });
-    if (category) return res.status(400).json({ msg: "This category name already exists." });
+    const duplicateCategory = await Categories.findOne({ name: currName });
+    if (duplicateCategory) return res.status(400).json({ msg: "This category name already exists." });
 
     //category 조회 후 업데이트
     await Categories.findOneAndUpdate({ name: oldName }, { name: currName });
@@ -27,7 +29,7 @@ const updateCategory = async (req: IReqAuth, res: Response) => {
     //post 조회 후 업데이트
     await Posts.updateMany({ category: oldName }, { $set: { category: currName } });
 
-    res.status(200).json({ msg: "Update Success!" });
+    res.status(200).json({ msg: "Updated successfully!" });
   } catch (err: any) {
     return res.status(500).json({ msg: err.message });
   }
--- server/src/controllers/blogCtrl/postCtrl/updatePost.ts
+++ server/src/controllers/blogCtrl/postCtrl/updatePost.ts
@@ -4,7 +4,7 @@ import Posts from "@models/blog/postModel";
 
 const updatePost = async (req: IReqAuth, res: Response) => {
   try {
-    //user가 middleware auth를 잘통과 했는지 확인
+    //user가 middleware auth를 통과 했는지 확인
     if (!req.user) return res.status(400).json({ msg: "Invalid Authorization." });
 
     //admin 인지 확인
@@ -12,6 +12,10 @@ const updatePost = async (req: IReqAuth, res: Response) => {
 
     //client 데이터 가져오기
     const { _id: postId, title, tags, content, thumbnail, description, category, privacy } = req.body;
+
+    if (!title) return res.status(400).json({ msg: "The title must be more than 1 character." });
+
+    if (title.length > 100) return res.status(400).json({ msg: "The category name must be less than 100 characters." });
 
     //post 조회 후 업데이트
     const post = await Posts.findOneAndUpdate(
@@ -30,7 +34,7 @@ const updatePost = async (req: IReqAuth, res: Response) => {
 
     if (!post) return res.status(400).json({ msg: "Invalid Authentication." });
 
-    res.status(200).json({ msg: "Update Success!" });
+    res.status(200).json({ msg: "Updated successfully!" });
   } catch (err: any) {
     return res.status(500).json({ msg: err.message });
   }
