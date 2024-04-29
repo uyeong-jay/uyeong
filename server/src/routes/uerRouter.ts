deec91f30e7ab45d1be3735fd1ddb97a35dbe952
william-jacob <wjacob2103@gmail.com>
2024-04-29 19:39:25 +0900

    comment: deleted useless comment

--- server/src/routes/uerRouter.ts
+++ server/src/routes/uerRouter.ts
@@ -5,11 +5,6 @@ import auth from "@middleware/auth";
 const userRouter = express.Router();
 const { updateUser } = userCtrl;
 
-// get(read): 데이터 조회, no need data
-// post(create): 데이터 등록
-// put(update): 데이터 변경(모두)
-// patch(update): 데이터 변경(부분)
-// delete(delete): 데이터 제거
 userRouter.patch("/user", auth, updateUser);
 
 export default userRouter;
