b55699dc68a63238eff1050f2532890a5040a0ef
william-jacob <wjacob2103@gmail.com>
2024-07-15 16:07:26 +0900

    feat: changed to clickable badges

--- client/src/components/pages/About/AboutComponents/Badges/Badges.tsx
+++ client/src/components/pages/About/AboutComponents/Badges/Badges.tsx
@@ -1,29 +1,65 @@
 import Image from 'next/image';
-import React from 'react';
-import { badgeData } from './BadgeData';
+import React, { useState } from 'react';
+import { badgeData as badges } from './BadgeData';
 import { DIV } from './BadgesStyle';
 
+interface ClickedBadges {
+  [key: string]: boolean; // 각 배지 이름에 해당하는 클릭 상태를 boolean으로 표현
+}
+
 const Badges = () => {
+  const [clickedBadges, setClickedBadges] = useState<ClickedBadges>({});
+  const [isAllBadgesActive, setAllBadgesActive] = useState(false);
+
+  const onClickBadge = (badgeName: string) => {
+    if (isAllBadgesActive) return;
+    setClickedBadges((prevState) => ({
+      ...prevState,
+      [badgeName]: !prevState[badgeName],
+    }));
+  };
+
+  const onClickAllBadgesInactive = () => {
+    if (!isAllBadgesActive) return;
+    setAllBadgesActive(false);
+    setClickedBadges({});
+  };
+
+  const onClickAllBadgesActive = () => {
+    if (isAllBadgesActive) return;
+    setAllBadgesActive(true);
+    setClickedBadges({});
+  };
+
   return (
-    <DIV.Frame>
-      {badgeData.map((v) => (
-        <div key={v.name}>
-          <p>- {v.name} -</p>
+    <DIV.Frame isAllBadgesActive={isAllBadgesActive}>
+      <button onClick={onClickAllBadgesInactive}></button>
+      <button onClick={onClickAllBadgesActive}></button>
+      {badges.map((badge) => (
+        <div key={badge.name}>
+          <p>- {badge.name} -</p>
           <ul>
             {/* front, back, devOps */}
             <li>
-              {v.contents.map((v) => (
-                /* badge */
-                <div key={v.name}>
-                  <Image
-                    width={v.width}
-                    height={20}
-                    alt={v.logoName}
-                    src={`https://img.shields.io/badge/${v.name}-${v.color}?style=plastic&logo=${v.logoName}&logoColor=white`}
-                    priority
-                  />
-                </div>
-              ))}
+              {badge.contents.map((badgeItem) => {
+                // badge detail
+                const isClicked = clickedBadges[badgeItem.name] ?? false; // 클릭 여부 확인
+                return isAllBadgesActive || isClicked ? (
+                  <div key={badgeItem.name} onClick={() => onClickBadge(badgeItem.name)}>
+                    <Image
+                      width={badgeItem.width}
+                      height={21}
+                      alt={badgeItem.logoName}
+                      src={`https://img.shields.io/badge/${badgeItem.name}-${badgeItem.color}?style=plastic&logo=${badgeItem.logoName}&logoColor=white`}
+                      priority
+                    />
+                  </div>
+                ) : (
+                  <div onClick={() => onClickBadge(badgeItem.name)} style={{ width: badgeItem.width, height: '27px' }}>
+                    <span>{badgeItem.name.replace('--', '-')}</span>
+                  </div>
+                );
+              })}
             </li>
           </ul>
         </div>
--- client/src/components/pages/About/AboutComponents/Badges/BadgesStyle.ts
+++ client/src/components/pages/About/AboutComponents/Badges/BadgesStyle.ts
@@ -2,13 +2,42 @@ import styled from '@_settings/styled';
 
 export const DIV = {} as any;
 
-DIV.Frame = styled.div`
+interface BadgesProps {
+  isAllBadgesActive: boolean;
+}
+
+DIV.Frame = styled.div<BadgesProps>`
   // border: 1px solid black;
   padding: 0 10px 10px 10px;
   // min-width: 100%;
 
+  & > button {
+    border: 2px solid ${({ theme }) => theme.BD_C};
+    position: absolute;
+    top: -25px;
+    height: 15px;
+    border-radius: 50%;
+  }
+
+  & > button:nth-of-type(1) {
+    background-color: ${(props) => {
+      if (!props.isAllBadgesActive) return `${props.theme.BD_C};`;
+      else return `${props.theme.BG_C};`;
+    }};
+    right: 50px;
+  }
+
+  & > button:nth-of-type(2) {
+    background-color: ${(props) => {
+      if (props.isAllBadgesActive) return `${props.theme.BD_C};`;
+      else return `${props.theme.BG_C};`;
+    }};
+    right: 20px;
+  }
+
   & > div {
     // border: 1px solid black;
+    // margin-bottom: 1px;
 
     & > p {
       // border: 1px solid red;
@@ -26,7 +55,20 @@ DIV.Frame = styled.div`
 
         & > div {
           // border: 1px solid black;
-          margin-right: 10px;
+          margin: 5px 10px 5px 0;
+          cursor: pointer;
+
+          & > span {
+            border: 1px solid ${({ theme }) => theme.BD_C};
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            &:hover {
+              color: ${({ theme }) => theme.FONT_C}99; //99: opacity 60%;
+            }
+          }
         }
       }
     }
