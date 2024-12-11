e08915d0f0f20cf9ac0cf09894c503eb9f3e224a
UYeong <uyeong.jay@gmail.com>
2024-12-11 18:11:01 +0900

    feat: added required meta tags

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -9,15 +9,17 @@ import { useCallback, useEffect, useMemo } from 'react';
 import { useCookies } from 'react-cookie';
 import cookieParser from '@utils/cookieParser';
 import '@_settings/fontStyles.css';
+import Head from 'next/head';
 
 const LIGHT_THEME = 'light';
 const DARK_THEME = 'dark';
 
 interface Props extends AppProps {
+  multiLangDescription: string;
   theme: typeof LIGHT_THEME | typeof DARK_THEME;
 }
 
-function MyApp({ Component, theme: themeInCookie, ...rest }: Props) {
+function MyApp({ Component, multiLangDescription, theme: themeInCookie, ...rest }: Props) {
   const { store, props } = wrapper.useWrappedStore(rest);
 
   const [cookies, setCookie] = useCookies(['theme']);
@@ -35,6 +37,23 @@ function MyApp({ Component, theme: themeInCookie, ...rest }: Props) {
     <>
       <ThemeProvider theme={theme === LIGHT_THEME ? lightTheme : darkTheme}>
         <Provider store={store}>
+          <Head>
+            <meta charSet="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes, viewport-fit=cover"
+            />
+            <meta name="description" content={multiLangDescription} />
+            <meta property="og:title" content="Uyeong" />
+            <meta property="og:description" content={multiLangDescription} />
+            <meta
+              property="og:image"
+              content="https://res.cloudinary.com/uyeong/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1733820910/uyeong-blog/uyeongblog_tm4nhd.png"
+            />
+            <meta property="og:url" content="https://uyeong.com" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Uyeong's Blog" />
+          </Head>
           <GlobalStyle />
           <AppLayout isDarkTheme={theme === DARK_THEME} onClickDarkMode={onClickDarkMode}>
             <Component {...props.pageProps} />
@@ -47,6 +66,12 @@ function MyApp({ Component, theme: themeInCookie, ...rest }: Props) {
 
 MyApp.getInitialProps = async ({ ctx }: AppContext) => {
   const cookies = cookieParser(ctx.req?.headers?.cookie);
-  return { theme: cookies.theme || LIGHT_THEME };
+  const lang = ctx.req?.headers['accept-language']?.split(',')[0] || 'en';
+
+  const multiLangDescription = lang.startsWith('ko')
+    ? '안녕하세요! 개발, 여행, 요리 등 제 다양한 일상들을 기록하는 블로그입니다. 방문해 주셔서 감사합니다. 좋은 하루 되세요!'
+    : 'Hi! Welcome to my blog where I share my experiences in development, travel, cooking, and everyday life. Thank you for visiting, and have a great day!';
+
+  return { multiLangDescription, theme: cookies.theme || LIGHT_THEME };
 };
 export default MyApp;
