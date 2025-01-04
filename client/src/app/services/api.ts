f4fb5ee2ea5757e7422667fe14841cb2dc89b754
UYeong <uyeong.jay@gmail.com>
2025-01-04 18:51:10 +0900

    comment: remove comments

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -4,12 +4,10 @@ import { axiosBaseQuery } from './baseQuery';
 import getConfig from 'next/config';
 const { serverRuntimeConfig } = getConfig();
 const SERVER_URL = serverRuntimeConfig.serverURL;
-// const API_URL: string = serverRuntimeConfig.apiURL || publicRuntimeConfig.apiURL;
 
 export const api = createApi({
   baseQuery: axiosBaseQuery({
     baseUrl: typeof window === 'undefined' ? SERVER_URL : process.env.NEXT_PUBLIC_BASE_URL,
-    // baseUrl: API_URL,
   }),
   extractRehydrationInfo(action, { reducerPath }) {
     if (action.type === HYDRATE) {
