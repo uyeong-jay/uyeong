02d511cf7874a31bf7ee5818d40db8fe7500944a
UYeong <uyeong.jay@gmail.com>
2025-01-04 17:14:31 +0900

    perf: make the font & badge images preloaded

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import { badgeData } from '@pages/About/AboutComponents/Badges/BadgeData';
 
 class MyDocument extends Document {
   static async getInitialProps(context: DocumentContext) {
@@ -12,11 +13,31 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&family=Square+Peg&family=Baloo+Bhaijaan+2&family=Sono&display=block"
             rel="stylesheet"
-          ></link>
+          />
+          <link rel="preload" href="/fonts/MaplestoryOTFBold.woff" as="font" type="font/woff" crossOrigin="anonymous" />
+          <link
+            rel="preload"
+            href="/fonts/GangwonEdu_OTFBoldA.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+
+          {/* badge images preload */}
+          {badgeData.flatMap((category) =>
+            category.contents.map((badge, index) => (
+              <link
+                key={`${category.name}-${index}`}
+                rel="preload"
+                href={`https://img.shields.io/badge/${badge.name}-${badge.color}?style=plastic&logo=${badge.logoName}&logoColor=white`}
+                as="image"
+              />
+            ))
+          )}
         </Head>
         <body>
           <Main />
